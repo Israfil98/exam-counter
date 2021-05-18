fb7483211b0b4b21607b45181afc3d27a8013e47
Israfil98 <israfil.guseynov.98@inbox.ru>
2021-05-18 18:30:19 +0400

    got prettier code

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,35 @@ import React, { useState } from 'react';
 
 import './App.css';
 
-import Display from "./components/Display/Display";
-import Button from "./components/Button/Button";
+import Display from './components/Display/Display';
+import Button from './components/Button/Button';
 
 function App() {
-    const [value, setValue] = useState<number>(0)
+    const [value, setValue] = useState<number>(0);
 
     const incrementValue = () => {
-        setValue(value + 1)
-    }
+        setValue(value + 1);
+    };
     const resetValue = () => {
-        setValue(0)
-    }
+        setValue(0);
+    };
 
     return (
         <div className="app-container">
             This is awesome counter!!!
             <div className="counter-wrapper">
-                <Display value={ value }/>
+                <Display value={value} />
                 <div className="buttons-wrapper">
-                    <Button title='inc' callback={ incrementValue } disabled={ value === 5 }/>
-                    <Button title='reset' callback={ resetValue } disabled={ value === 0 }/>
+                    <Button
+                        title="inc"
+                        callback={incrementValue}
+                        disabled={value === 5}
+                    />
+                    <Button
+                        title="reset"
+                        callback={resetValue}
+                        disabled={value === 0}
+                    />
                 </div>
             </div>
         </div>
